dda9f82f74f08fdc2dd66bddfcc49b7b78217b2b
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-04-03 11:18:37 +0530

    Add files via upload

---
+++ positiveNegative.js
@@ -0,0 +1,17 @@
+// Problem 5:
+//  Write a JavaScript program that assigns a value to a variable representing a 
+// number and checks whether it is positive. If it is positive, print "Positive"; 
+// otherwise, print "Not positive".
+
+var a ;
+a=parseInt(prompt("Enter Any Number : "));
+if(a<0){
+    alert("The sign of product is : - (Negative) ")
+}
+else if (a==0){
+    alert("The sign of product is : None , Because it is zero ")
+}
+else{
+    alert("The sign of product is : + (Positive) ")
+
+}
