610408e6c94fa29f0d487a0cc606b1d2d7342052
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-04-03 11:17:51 +0530

    Add files via upload

---
+++ evenodd.js
@@ -0,0 +1,14 @@
+// Problem 4:
+// Develop a program in JavaScript that assigns a value to a variable representing 
+// a number and checks whether it is even or odd.
+
+var n=parseInt(prompt("Enter Any Number : "));
+if(n%2==0){
+    console.log("Given Number Is even Number.")
+}
+else if (n===0){
+    console.log("Given Number Is 'Zero'. ")
+}
+else{
+    console.log("Given Number Is odd Number. ")
+}
