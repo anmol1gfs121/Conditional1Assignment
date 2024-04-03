29c2920a08172f0a8f59fe0e7a4679d258919e75
anmol1gfs121 <92971089+anmol1gfs121@users.noreply.github.com>
2024-04-03 11:17:33 +0530

    Add files via upload

---
+++ divisibleby5.js
@@ -0,0 +1,12 @@
+// Problem 3:
+// Develop a program in JavaScript that assigns a value to a variable and checks 
+// whether it is divisible by 5, printing "Divisible by 5" or "Not divisible by 5" 
+// accordingly.
+
+var n=parseInt(prompt("Enter Any Number : "));
+if(n%5==0){
+    console.log("Given Number Is A divisible Of Five")
+}
+else{
+    console.log("Given Number Is Not A divisible Of Five")
+}
